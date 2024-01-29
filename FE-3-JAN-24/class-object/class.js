359c6f3dc7e1e92d5fdbb16a1ac1dbac40831f37
Alok3333 <alokprajapati697@gmail.com>
2024-01-29 17:31:25 +0530

    created class and constructor-function we used property and methods in javascript (oops concept)

--- FE-3-JAN-24/class-object/class.js
+++ FE-3-JAN-24/class-object/class.js
@@ -47,29 +47,77 @@
 // ************* Inheritance(extends) *************
 
 // Parent Class
-class Student {
-    // Parent class contructor with parameter
-    constructor(name) {
-        this.fullName = name;
-    }
+// class Student {
+//     // Parent class contructor with parameter
+//     constructor(name) {
+//         this.fullName = name;
+//     }
 
-    // Parent class member method
-    markAttendance() {
-        console.log(this.fullName, " is Present today.");
-    }
+//     // Parent class member method
+//     markAttendance() {
+//         console.log(this.fullName, " is Present today.");
+//     }
+// };
+
+// let parent = new Student("Alok");
+// console.log(parent);
+
+// // Derived or Child Class
+// class SportsCaptain extends Student {
+//     // Child Class member method
+//     playFootball() {
+//         // fullName is derived from parent class's fullName data member
+//         console.log(this.fullName, " is playing football.");
+//     }
+// }
+
+// let child = new SportsCaptain();
+// console.log(child.playFootball());
+
+// ---------------Schema/Class---------------
+
+class Person {
+    name;
+    standard;
+    // subject;
+
+    constructor(nm, std, subject) {
+        this.name = nm;
+        this.standard = std;
+        this.sub = subject;
+    };
+
+    canWalk() {
+        return `${this.name} can walk`;
+    };
+
+    subjects() {
+        return `${this.sub} this is your subject name`;
+    };
 };
 
-let parent = new Student("Alok");
-console.log(parent);
+const utkarshObj = new Person("utkarsh kumar", "9th", "Math");
+console.log(utkarshObj);
+// console.log(utkarshObj.canWalk());
+// console.log(utkarshObj.subjects());
 
-// Derived or Child Class
-class SportsCaptain extends Student {
-    // Child Class member method
-    playFootball() {
-        // fullName is derived from parent class's fullName data member
-        console.log(this.fullName, " is playing football.");
-    }
-}
+// ---------------Constructor-Function---------------
 
-let child = new SportsCaptain();
-console.log(child.playFootball());
+function Student(studentName, studentStandard, favourateSubject) {
+    this.name = studentName;
+    this.standard = studentStandard;
+    this.favourateSub = favourateSubject;
+
+    this.intro = function() {
+        return `Good evening one and all my name is ${this.name}`;
+    };
+
+    this.favSub = function() {
+        return `My favourate subject is ${this.favourateSub}`;
+    };
+};
+
+const alokObj = new Student("Alok kumar", "10th", "Algebra");
+console.log(alokObj);
+console.log(alokObj.intro());
+console.log(alokObj.favSub());
